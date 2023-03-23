354a092e83e4335b2ea180a017964568257c17bd
yusufaliselek <yusufaliselek41@gmail.com>
2023-03-23 13:00:32 +0300

    Add project files.

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        IQueryable<T> Where(Expression<Func<T,bool>> predicate); // sorgu yazılacaksa iquery
+
+        Task AddAsync(T entity);
+
+        void Remove(T entity);
+
+        T Update(T entity);
+    }
+}
---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int Status { get; private set; }
+
+        [JsonIgnore]
+        public bool isSuccessful { get; private set; }
+
+        public ErrorDto Error { get; private set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, Status = statusCode, isSuccessful=true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data=default, Status = statusCode, isSuccessful = true };
+        }
+
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new Response<T> 
+            { 
+                Error = errorDto, 
+                Status = statusCode,
+                isSuccessful = false
+            };
+        }
+
+        public static Response<T> Fail(string message, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(message, isShow);
+
+             return new Response<T> { Error = errorDto, Status = statusCode, isSuccessful = false };
+        }
+
+    }
+}
